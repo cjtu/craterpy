ab2711fe98c9c2f85fd9439c9535658abafe83a7
Cjtu <cjtai@rogers.com>
2017-07-04 13:44:36 -0400

    Moved setup flags to setup file

--- acerim/version.py
+++ acerim/version.py
@@ -1,11 +1,8 @@
-from __future__ import absolute_import, division, print_function
-from os.path import join as pjoin
-
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 _version_major = 0
 _version_minor = 2
 _version_micro = ''  # use '' for first of series, number for 1 and above
-_version_extra = '' # make it '' for full release
+_version_extra = '' # dev for in development (NEVER push with dev), '' for full release
 
 # Construct full version string from these.
 _ver = [_version_major, _version_minor]
@@ -15,63 +12,3 @@ if _version_extra:
     _ver.append(_version_extra)
 
 __version__ = '.'.join(map(str, _ver))
-
-CLASSIFIERS = ["Development Status :: 2 - Pre-Alpha",
-               "Environment :: Console",
-               "Intended Audience :: Science/Research",
-               "License :: OSI Approved :: MIT License",
-               "Natural Language :: English",
-               "Operating System :: OS Independent",
-               "Programming Language :: Python",
-               "Topic :: Scientific/Engineering"]
-
-# Description should be a one-liner:
-description = "acerim: a package for analyzing impact crater ejecta"
-# Long description will go up on the pypi page
-long_description = """
-
-Acerim
-=======
-The Automated Crater Ejecta Region of Interest Mapper (ACERIM) is a python 
-package for analyzing impact crater ejecta.
-
-This package provides a variety of tools for:
-- loading crater data
-- loading image data
-- locating craters in an image
-- extracting a Region of Interest (ROI) around a crater from an image
-- analyzing ROIs using the included or user-defined statistical methods
-
-
-To get started using this software, please go to the repository README_.
-
-.. _README: https://github.com/cjtu/acerim/master/README.md
-
-License
-=======
-``acerim`` is licensed under the terms of the MIT license. See the file
-"LICENSE" for information on the history of this software, terms & conditions
-for usage, and a DISCLAIMER OF ALL WARRANTIES.
-
-All trademarks referenced herein are property of their respective holders.
-
-Copyright (c) 2017--, Christian Tai Udovicic.
-"""
-
-NAME = "acerim"
-MAINTAINER = "Christian Tai Udovicic"
-MAINTAINER_EMAIL = "cj.taiudovicic@gmail.com"
-DESCRIPTION = description
-LONG_DESCRIPTION = long_description
-URL = "http://github.com/cjtu/acerim"
-DOWNLOAD_URL = ""
-LICENSE = "MIT"
-AUTHOR = "Christian Tai Udovicic"
-AUTHOR_EMAIL = "cj.taiudovicic@gmail.com"
-PLATFORMS = "OS Independent"
-MAJOR = _version_major
-MINOR = _version_minor
-MICRO = _version_micro
-VERSION = __version__
-PACKAGE_DATA = {'acerim': [pjoin('data', '*')]}
-REQUIRES = ["numpy", "scipy", "matplotlib", "gdal", "pandas"]
--- setup.py
+++ setup.py
@@ -1,30 +1,74 @@
-import os
+""" Use setuptools to prepare acerim package.
+"""
 from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
 PACKAGES = find_packages()
 
-# Get version and release info, which is all stored in acerim/version.py
-ver_file = os.path.join('acerim', 'version.py')
-with open(ver_file) as f:
-    exec(f.read())
+# Get version from acerim/version.py
+version = {}
+with open(path.join(here, 'acerim','version.py')) as f:
+    exec(f.read(), version)
 
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            licens=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=PACKAGES,
-            package_data=PACKAGE_DATA,
-            install_requires=REQUIRES,
-            requires=REQUIRES)
+setup(
+    name='acerim',
+    version=version['__version__'], # From /acerim/version.py
+    description='A package for analyzing impact crater ejecta',
+    long_description="""
+        Acerim
+        =======
+        The Automated Crater Ejecta Region of Interest Mapper (ACERIM) is a python 
+        package for analyzing impact crater ejecta.
 
+        This package provides a variety of tools for:
+        - loading crater data
+        - loading image data
+        - locating craters in an image
+        - extracting a Region of Interest (ROI) around a crater from an image
+        - analyzing ROIs using the included or user-defined statistical methods
 
-if __name__ == '__main__':
-    setup(**opts)
+        To get started using this software, please go to the repository README_.
+
+        .. _README: https://github.com/cjtu/acerim/master/README.md
+
+        License
+        =======
+        ``acerim`` is licensed under the terms of the MIT license. See the file
+        "LICENSE" for information on the history of this software, terms & conditions
+        for usage, and a DISCLAIMER OF ALL WARRANTIES.
+
+        All trademarks referenced herein are property of their respective holders.
+
+        Copyright (c) 2017--, Christian Tai Udovicic.
+    """,    
+    maintainer='Christian Tai Udovicic',
+    maintainer_email='cj.taiudovicic@gmail.com',     
+    url='http://github.com/cjtu/acerim',
+    download_url='',
+    license='MIT',
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Scientific/Engineering"
+    ],
+    author='Christian Tai Udovicic',
+    author_email='cj.taiudovicic@gmail.com',
+    platforms='OS Independent',     
+    packages=PACKAGES,
+    package_data={
+        'tests': ['craters.csv', 'moon.tif'],
+        'examples': ['craters.csv', 'moon.tif'],
+    },
+    install_requires=['numpy', 'scipy', 'matplotlib', 'gdal', 'pandas'],
+    extras_require={
+        'testing': ['pytest'],
+        'citation': ['duecredit'],
+    }
+)
