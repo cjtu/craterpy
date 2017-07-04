2fb0541bcef046e2a521db1635517868a04115e3
Cjtu <cjtai@rogers.com>
2017-07-04 12:42:19 -0400

    Rm extraneous imports

--- acerim/__init__.py
+++ acerim/__init__.py
@@ -6,14 +6,3 @@ Created on Mon May 22 09:29:08 2017
 """
 
 from __future__ import absolute_import, division, print_function
-from .version import __version__
-from .acerim import *
-
-if __name__ == '__main__':
-    import gdal
-    import pandas as pd
-    import numpy as np
-    import matplotlib.pyplot as plt
-    import classes as ac
-    import functions as af
-    import acestats as acs
