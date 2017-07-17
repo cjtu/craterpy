543f3b64ead93c77be5de1a15cd65ff45720278c
Christian Tai Udovicic <cj.taiudovicic@gmail.com>
2017-07-17 12:23:39 -0400

    Update to 0.0.6dev

--- acerim/version.py
+++ acerim/version.py
@@ -1,8 +1,8 @@
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 _version_major = 0
 _version_minor = 0
-_version_maintenance = 5  # use '' for first of series, number for 1 and above
-_version_extra = '' # dev for in development (NEVER push with dev), '' for full release
+_version_maintenance = 6  # use '' for first of series, number for 1 and above
+_version_extra = 'dev' # dev for in development (NEVER push with dev), '' for full release
 
 # Construct full version string from these.
 _ver = [_version_major, _version_minor]
