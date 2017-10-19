fe029c8678f6f431c732fe8d1c8bf15caa626f92
Christian <cj.taiudovicic@gmail.com>
2017-10-19 00:39:25 -0400

    Fix osx travis build

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-
-    # update stuff
-    if [[ "$TRAVIS_PYTHON_VERSION" == "py27" ]]; then
+    if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
         wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh
     else
         wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
