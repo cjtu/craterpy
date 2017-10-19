bac2f4b92a156543cb4faa189caffc3fa534a744
Christian <cj.taiudovicic@gmail.com>
2017-10-18 23:12:29 -0400

    Fix path issue

--- travis/before_install.sh
+++ travis/before_install.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
 
     # update stuff
-    case "${TOXENV}" in
-        py27)
-            wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh
-            ;;
-        py34)
-            wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh
-            ;;
-    esac
+    if [[ "$TRAVIS_PYTHON_VERSION" == "py27" ]]; then
+        wget https://repo.continuum.io/miniconda/Miniconda2-latest-MacOSX-x86_64.sh -O miniconda.sh
+    else
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+    fi    
 else
     if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
-      wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
+        wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
     else
-      wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+        wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
     fi
 fi
+
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH=$HOME/miniconda/bin:$PATH
+hash -r
+conda config --set always_yes yes --set changeps1 no
+conda config --set anaconda_upload no
+conda update -q conda
+conda info -a
+conda create -q -n test-env python=$TRAVIS_PYTHON_VERSION
