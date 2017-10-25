7c2342a25d901b1b77102c4b7d8b53b374fdbace
cjtu <cj.taiudovicic@gmail.com>
2017-10-25 18:10:57 -0400

    Add roi tests

--- craterpy/tests/test_roi.py
+++ craterpy/tests/test_roi.py
@@ -4,7 +4,7 @@ import unittest
 import numpy as np
 from craterpy.dataset import CraterpyDataset
 from craterpy.roi import CraterRoi
-
+from craterpy.masking import circle_mask
 
 class TestCraterRoi(unittest.TestCase):
     """Test CraterpyDataset object"""
@@ -30,3 +30,28 @@ class TestCraterRoi(unittest.TestCase):
         actual = roi._get_extent()
         expected = (19.4384, 20.5616, 19.4722, 20.5278)
         np.testing.assert_almost_equal(actual, expected, 4)
+
+    def test_get_roi(self):
+        roi = CraterRoi(self.cds, 0, 0, 50)
+        arr = roi.get_roi()
+        self.assertIsNotNone(arr)
+        roi180 = CraterRoi(self.cds, 0, 180, 50)
+        arr180 = roi.get_roi()
+        self.assertIsNotNone(arr180)
+
+    def test_filter(self):
+        roi = self.roi
+        roi.filter(0, 100)
+        self.assertTrue(roi.roi.max() <= 100)
+        roi.filter(0, 50, strict=True)
+        self.assertTrue(roi.roi.max() < 50)
+
+    def test_mask(self):
+        shape, rad = self.roi.roi.shape, self.roi.rad
+        mask = circle_mask(shape, rad)
+        masked_roi = self.roi.mask(mask)
+        self.assertTrue(np.isnan(self.roi.roi[*(shape/2)]))
+
+    def test_plot(self):
+        self.assertIsNotNone(self.roi.plot())
+
