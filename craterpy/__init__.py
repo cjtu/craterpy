720a839975dcef1285a0fe3db9aa7d09a0deb99d
Christian J. Tai Udovicic <cj.taiudovicic@gmail.com>
2022-07-29 15:50:08 -0700

    Run linting and update docs

--- craterpy/__init__.py
+++ craterpy/__init__.py
@@ -1,10 +1,6 @@
 """craterpy module"""
+from importlib import metadata
 from craterpy.dataset import CraterpyDataset
 from craterpy.roi import CraterRoi
-
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
-
-__version__ = importlib_metadata.version(__package__)
+__version__ = metadata.version(__package__)
+del metadata
