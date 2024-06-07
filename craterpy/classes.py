c5f73b0652299ba9d7e37fa3bba0150bc6f2fbf1
Christian J. Tai Udovicic <cj.taiudovicic@gmail.com>
2024-06-06 14:20:59 -1000

    Generate annuli in local projection

--- craterpy/classes.py
+++ craterpy/classes.py
@@ -1,7 +1,12 @@
 import pandas as pd
 import geopandas as gpd
-from shapely.geometry import Point, MultiPolygon
+from shapely.geometry import Point
+from pyproj import CRS, Transformer
+from pyproj.crs import ProjectedCRS
+from pyproj.crs.coordinate_operation import AzimuthalEquidistantConversion
+from shapely.ops import transform
 import craterpy.helper as ch
+from rasterstats import gen_zonal_stats
 
 # TODO: decide how to handle geodataframe annuli
 # Geopandas only allow 1 geometry per row, so we can't have annuli and craters in the rows
@@ -9,34 +14,95 @@ import craterpy.helper as ch
 # Can it be a multiindex somehow? or multipolygon - don't think that works with rasterstats?
 #  Thought: keep crater point geom and compute poly on the fly?
 
-class CraterData:
-    def __init__(self, filepath, crs='', annuli=(1, 2)):
+
+class CraterDatabase:
+    def __init__(self, filepath, crs=''):
         self.filepath = filepath
-        self.crs = crs
-        self.annuli = annuli
-        self._load_data()
-        self.latcol, self.loncol, self.radcol = ch.get_crater_cols(self.data)
-
-    def _load_data(self):
-        if self.filepath.endswith('.csv'):
-            df = pd.read_csv(self.filepath)
-            lat, lon, rad = ch.get_crater_cols(df)
-            geometry = [Point(xy).buffer(radius) for xy, radius in zip(zip(df[lon], df[lat]), df[rad])]
-            self.data = gpd.GeoDataFrame(df, geometry=geometry, crs=self.crs)
-        else:
-            self.data = gpd.read_file(self.filepath, crs=self.crs)
-
-    def _generate_annuli(self):
-        """Generate annulus for each row in dataframe."""
-        radii = self.data[self.radcol]
-        center = Point(self.data[self.loncol], self.data[self.latcol])
-        annuli = center.buffer(outer*radii).difference(center.buffer(inner*radii))
-
-
-
+        self.crs = CRS.from_user_input(crs)
+        self.data = self._load_data()
+        self.geometry = {}  # Store generated geometries here
 
     def __repr__(self):            
         return self.data.__repr__()
     
+    def _load_data(self):
+        """Load crater database from file."""
+        gdf = gpd.read_file(self.filepath)
+        self.latcol, self.loncol, self.radcol = ch.get_crater_cols(gdf)
+        for col in [self.latcol, self.loncol, self.radcol]:
+            gdf[col] = pd.to_numeric(gdf[col])
+        if isinstance(gdf, pd.core.frame.DataFrame):
+            geometry = [Point(xy) for xy in zip(gdf[self.loncol], gdf[self.latcol])]
+            gdf = gpd.GeoDataFrame(gdf, geometry=geometry, crs=self.crs)
+        return gdf
+
+    def _generate_point(self):
+        
+        pass
+
+    def _generate_rim(self):
+        pass
+
+    def _generate_floor(self):
+        pass
+
+    def _generate_peak(self):
+        pass
+
+    def _generate_ejecta(self):
+        pass
+
+    def _generate_annulus(self, inner, outer):
+        """Generate annulus for each row in dataframe."""
+        rims = []
+        annuli = []
+        for _, row in self.data.iterrows():
+            center = row['geometry']
+            radius = row[self.radcol] * 1000 # [km] -> [m]
+
+            # Create a local azimuthal equidistant projection for the crater
+            local_crs = ProjectedCRS(
+                name=f'AzimuthalEquidistant({center.y:.2f}N, {center.x:.2f}E)', 
+                conversion=AzimuthalEquidistantConversion(center.y, center.x), 
+                geodetic_crs=self.crs)
+
+            # Old way with proj4 strings / dict (bad)
+            # local_crs_dict = self.crs.to_dict()
+            # local_crs_dict['proj'] = 'aeqd'
+            # local_crs_dict['lat_0'] = center.y
+            # local_crs_dict['lon_0'] = center.x
+            # local_crs = CRS.from_dict(local_crs_dict)
+            # local_crs = CRS.from_string(f"+proj=aeqd +lat_0={center.y} +lon_0={center.x} +x_0=0 +y_0=0 +datum={self.crs.datum} +ellps={self.crs.ellipsoid}")
+            
+            # Create circle in crater azimuthally centered projection (0,0)
+            rim = Point(0, 0).buffer(radius)
+            inner_edge = Point(0, 0).buffer(radius * inner)
+            outer_edge = Point(0, 0).buffer(radius * outer)
+            annulus = outer_edge.difference(inner_edge)
+            
+            # Project the annulus back to the original CRS
+            transformer = Transformer.from_crs(local_crs, self.crs, always_xy=True)
+            annulus = transform(transformer.transform, annulus)
+            rim = transform(transformer.transform, rim)
+
+            # # Create transformers for the forward and reverse transformations
+            # transformer_to_local = Transformer.from_crs(self.crs, local_crs, always_xy=True)
+            # transformer_from_local = Transformer.from_crs(local_crs, self.crs, always_xy=True)
+            annuli.append(annulus)
+            rims.append(rim)
+        self.data['rim'] = gpd.GeoSeries(rims, index=self.data.index)
+        self.data['annulus'] = gpd.GeoSeries(annuli, index=self.data.index)
+
+    def zonal_stats(self, rasters, roi='ejecta', **kwargs):
+        # TODO: write and test this
+        if roi == 'ejecta':
+            tmpdf = self.data['annulus'].rename({'annulus': 'geometry'})
+            
+        # Yields generator - figure out where we want to store all the data
+        for raster in rasters:
+            out = gen_zonal_stats(tmpdf, raster, **kwargs)
+            break
+
+        return list(out)
 
 
