e264d63408040f458cc5ece68656abe150fefeb5
Ari Essunfeld <ariessunfeld@gmail.com>
2025-04-14 12:37:13 -0700

    removing unused modules

--- craterpy/__init__.py
+++ craterpy/__init__.py
@@ -1,7 +1,6 @@
 """craterpy module"""
 
 from importlib import metadata
-from craterpy.roi import CraterRoi
 from craterpy.classes import CraterDatabase
 
 __version__ = metadata.version(__package__)
