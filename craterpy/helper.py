651e94e63b2d4113afe75aa62904a261dc830776
Ari Essunfeld <ariessunfeld@gmail.com>
2025-06-12 21:32:38 -0700

    add spacing to fix example syntax

--- craterpy/helper.py
+++ craterpy/helper.py
@@ -155,8 +155,10 @@ def greatcircdist(lat1, lon1, lat2, lon2, radius):
     Examples
     --------
     .. code-block:: doctest
+
         >>> greatcircdist(36.12, -86.67, 33.94, -118.40, 6372.8)
         2887.259950607111
+
     """
     # Convert degrees to radians
     lat1, lon1, lat2, lon2 = np.radians([lat1, lon1, lat2, lon2])
@@ -191,12 +193,14 @@ def inglobal(lat, lon):
     Examples
     --------
     .. code-block:: doctest
+
         >>> inglobal(0, 0)
         True
         >>> inglobal(91, 0)
         False
         >>> inglobal(0, -50)
         True
+
     """
     return (-90 <= lat <= 90) and (0 <= lon360(lon) <= 360)
 
@@ -246,6 +250,7 @@ def findcol(df, names, exact=False):
     Examples
     --------
     .. code-block:: doctest
+
         >>> df = pd.DataFrame({'Lat': [10, -20., 80.0],
                             'Lon': [14, -40.1, 317.2],
                             'Diam': [2, 12., 23.7]})
@@ -257,6 +262,7 @@ def findcol(df, names, exact=False):
         ValueError: No column containing ['Radius'] found.
         >>> findcol(df, 'diam')
         'Diam'
+        
     """
     if isinstance(names, str):
         names = [names]
@@ -329,8 +335,10 @@ def latlon_to_cartesian(lat, lon, radius):
     Examples
     --------
     .. code-block:: doctest
+
         >>> latlon_to_cartesian(0, 0, 1)
         (1.0, 0.0, 0.0)
+
     """
     lat, lon = np.radians(lat), np.radians(lon)
     x = radius * np.cos(lat) * np.cos(lon)
