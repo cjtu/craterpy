4ac9abdb4a0a72ab846ea9f704132b95ae28e41f
Ari Essunfeld <ariessunfeld@gmail.com>
2025-06-15 13:28:31 -1000

    add moon_dem.tif to package data

--- craterpy/__init__.py
+++ craterpy/__init__.py
@@ -5,7 +5,7 @@ from craterpy.classes import CraterDatabase
 
 sample_data = {
     k: str(resources.files(__name__).joinpath("data", k))
-    for k in ("moon.tif", "moon_craters.csv", "vesta.tif", "vesta_craters.csv")
+    for k in ("moon.tif", "moon_craters.csv", "vesta.tif", "vesta_craters.csv", "moon_dem.tif")
 }
 
 __version__ = metadata.version(__package__)
